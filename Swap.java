00cc5f1afcb26992b23492f35806e4eaf5487197
Harsh Lakhanpal <92823514+Kakashi1000Hatake@users.noreply.github.com>
2022-04-08 01:24:32 +0530

    Swap
    This program shows how to swap the values of the attributes.

---
+++ Swap.java
@@ -0,0 +1,21 @@
+public class Swap {
+    public static void main(String args[]){
+
+        int a = 10;
+        int b = 20;
+        int c;
+
+        // a = 10;
+        // b = 20;
+
+        c = a;
+        a=b;
+        b= c;
+
+        System.out.println("a: "+a);
+        System.out.println("b: "+b);
+
+
+
+    }
+}
