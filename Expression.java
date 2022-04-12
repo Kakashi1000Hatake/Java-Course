1815f32ca00f9884c00764eb2e0847ad3eb1fd5a
Harsh Lakhanpal <92823514+Kakashi1000Hatake@users.noreply.github.com>
2022-04-12 21:09:43 +0530

    Expressions
    this program file is about the expressions in java

---
+++ Expression.java
@@ -0,0 +1,29 @@
+public class Expression {
+    public static void main(String args[]){
+        // expression = operands and operators
+        // operands = values, variables, numbers, quantity
+        // operators = + - * / % 
+
+        // this is our main integer
+        int a = 20;
+        // this is for addition
+        int b = a + 10; 
+        // this is for subtraction
+        int c = a - 10;
+        // this is for multiplication
+        int d = a * 3;
+        // this is for division
+        int e = a / 2;
+        // this is for the remainder
+        int f = a % 2;
+
+        // this is for the outputs of our expressions
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+
+    }
+}
